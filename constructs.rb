21cd6af5bad5114083741e180610810db8df9381
scapegoatee <scapegoatee@protonmail.com>
2019-03-03 03:50:18 +0530

    Ruby Constructs Updated

--- constructs.rb
+++ constructs.rb
@@ -1,7 +1,7 @@
 class FooBar < BasicObject
     def self.gun
     end
-    #==
+    # ==
     class << self
         def gun
         end
@@ -11,8 +11,32 @@ end
 def foo_bar
 end
 
-lambda do
+-> x { puts x }     # == proc { |x| puts x }
+
+def foo(&blk) # & converts do..end into proc objects.
+    x = 'foo'
+    blk.call # => 'bar'
 end
+
+def bar
+    x = 'bar'
+    foo do
+        puts x # This will be executed in bar's context, not in foo's.
+    end
+end
+
+# 0 arity means that the function takes no arguments.
+# & can be used as a shorthand for 0 arity functions responding to the given variable.
+[1, 2, 3].map { |x| x.even? }
+[1, 2, 3].map(&:even?)
+
+# The following cant be shorthanded because they are not 0 arity.
+[1, 2, 3].map { |x| x.equal?(4) }
+
+# The following cant be shorthanded because they do not respond to given variable,
+# i.e. They dont follow the object.function pattern for a given variable x.
+
+[1, 2, 3].map { |x| x * 2 }
 
 if
 elsif
@@ -22,7 +46,9 @@ case
 when
 end
 
+
 1 * 1 == 1.*(1)
 
 def foo(*vars)
-  a, b = vars  # 'n' number of arguments can be placed here. eg. a, b, c, d,.., = vars
+  a, b,..,n = vars  # 'n' number of arguments can be placed here. eg. a, b, c, d,.., = vars. This auto-assigns vars into an array.
+end
